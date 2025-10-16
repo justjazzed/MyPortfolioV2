c4f418452a1f79b110a4f135abcce498a4429a64
Jasbar Legaspina <brailjazz@gmail.com>
2025-10-16 21:03:11 +0800

    Added another section for Servicec and photos

--- js/scripts.js
+++ js/scripts.js
@@ -164,3 +164,13 @@ document.querySelectorAll('.tablet-carousel-container').forEach(container => {
   nextBtn.addEventListener('click', slideNext);
   prevBtn.addEventListener('click', slidePrev);
 });
+
+//****Scroll animation for service */
+document.addEventListener("scroll", () => {
+  document.querySelectorAll('.fade-in-up').forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.classList.add('visible');
+    }
+  });
+});
